0c6db3351d309cf26651c6a7154ff27badbe80f1
Yash Sharma <yashsharmaji8502@gmail.com>
2026-07-04 20:54:57 +0530

    Some fixes

--- civicai/src/app/login/page.jsx
+++ civicai/src/app/login/page.jsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import React, { useState, useEffect, Suspense } from 'react';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
